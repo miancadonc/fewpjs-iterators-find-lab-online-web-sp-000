62ed56d776894a25294a17289c7f8ff9eceffe75
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-27 18:13:21 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(objectArray){
+  const winYear = objectArray.find(obj => {
+    return obj.result === "W"
+  })
+
+  return (!!winYear) ? winYear.year : undefined
+}
